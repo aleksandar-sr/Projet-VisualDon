eb60a16c174eede641e5dc0224dd3475cf186533
Aleksandar Srbinovski <aleksandar.srbinovski@heig-vd.ch>
2022-04-08 13:30:09 +0200

    Modif index

--- src/index.js
+++ src/index.js
@@ -6,22 +6,21 @@ const data = file;
 
 console.log(data[0]);
 
-
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
-    
 // append the svg object to the body of the page
-const svg = d3.select("#my_dataviz")
+const svg = d3
+  .select("#my_dataviz")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+  .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    /*
+/*
 
 /*Interactive line chart 
 
@@ -98,40 +97,53 @@ d3.select("#selectButton").on("change", function(d) {
 
 //Line chart
 
-  // group the data: I want to draw one line per group
-  const sumstat = d3.group(data, d => d.name); // nest function allows to group the calculation per level of a factor
+const sumstat = d3.group(data, (d) => d.name); // nest function allows to group the calculation per level of a factor
 
-  // Add X axis --> it is a date format
-  const x = d3.scaleLinear()
-    .domain(d3.extent(data, function(d) { return d.Year; }))
-    .range([ 0, width ]);
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).ticks(5));
+// Add X axis
+const x = d3
+  .scaleLinear()
+  .domain(
+    d3.extent(data, function (d) {
+      return d.Year;
+    })
+  )
+  .range([0, width]);
+svg
+  .append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(d3.axisBottom(x).ticks(5));
 
-  // Add Y axis
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return +d.Global_Sales; })])
-    .range([ height, 0 ]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+// Add Y axis
+const y = d3
+  .scaleLinear()
+  .domain([
+    0,
+    d3.max(data, function (d) {
+      return +d.Global_Sales;
+    }),
+  ])
+  .range([height, 0]);
+svg.append("g").call(d3.axisLeft(y));
 
-  // color palette
-  const color = d3.scaleOrdinal()
-    .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
-
-  // Draw the line
-  svg.selectAll(".line")
-      .data(sumstat)
-        .attr("fill", "none")
-        .attr("stroke", function(d){ return color(d[0]) })
-        .attr("stroke-width", 1.5)
-        .attr("d", function(d){
-          return d3.line()
-            .x(function(d) { return x(d.year); })
-            .y(function(d) { return y(+d.n); })
-            (d[1])
-        })
+// Draw the line
+svg
+  .selectAll(".line")
+  .data(sumstat)
+  .attr("fill", "none")
+  .attr("stroke", function (d) {
+    return color(d[0]);
+  })
+  .attr("stroke-width", 1.5)
+  .attr("d", function (d) {
+    return d3
+      .line()
+      .x(function (d) {
+        return x(d.year);
+      })
+      .y(function (d) {
+        return y(+d.n);
+      })(d[1]);
+  });
 
 /*
 Exemple
